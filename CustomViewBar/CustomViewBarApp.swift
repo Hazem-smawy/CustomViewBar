011685bb42c2a2755c8d9237519fae5739881cdc
hazem-smawy <hazemsmawy@gmail.com>
2022-09-29 11:10:48 +0300

    Initial Commit

---
+++ CustomViewBar/CustomViewBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomViewBarApp.swift
+//  CustomViewBar
+//
+//  Created by hazem smawy on 9/29/22.
+//
+
+import SwiftUI
+
+@main
+struct CustomViewBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
